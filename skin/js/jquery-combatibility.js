47bee0d9a126cf20e3b210a895d5a6f7a5812585
ReneMuetti <ReneMuetti@googlemail.com>
2026-04-08 20:20:23 +0200

    * upgrade jQuery UI from 1.14.0b2 to 1.14.2

--- skin/js/jquery-combatibility.js
+++ skin/js/jquery-combatibility.js
@@ -5,23 +5,27 @@
 (function($) {
     if (typeof $ === 'undefined') return;
 
-    // 1. Statische Methoden auf dem $-Objekt
+    // 1. Bootstrap 3 Trick: Version vorgaukeln
+    $.fn.jquery = "3.7.1";
+
+    // 2. Statische Methoden auf dem $-Objekt
     if (!$.isArray)    $.isArray = Array.isArray;
     if (!$.isFunction) $.isFunction = (obj) => typeof obj === 'function';
     if (!$.isWindow)   $.isWindow = (obj) => obj != null && obj === obj.window;
     if (!$.type)       $.type = (obj) => Object.prototype.toString.call(obj).replace(/^\[object (.+)\]$/, "$1").toLowerCase();
+    if (!$.now)        $.now = Date.now;
 
     if (!$.camelCase) {
         $.camelCase = (str) => str.replace(/-([a-z])/g, (g) => g[1].toUpperCase());
     }
 
-    // 2. Methoden für jQuery-Instanzen (Prototyp)
+    // 3. Methoden für jQuery-Instanzen (Prototyp)
     // Dies fixiert den "a.sort is not a function" Fehler
     if (!$.fn.sort) {
         $.fn.sort = Array.prototype.sort;
     }
 
-    // 3. $.swap wird oft für Dimensionen in alten Plugins genutzt
+    // 4. $.swap wird oft für Dimensionen in alten Plugins genutzt
     if (!$.swap) {
         $.swap = function(elem, options, callback, args) {
             var ret, name, old = {};
